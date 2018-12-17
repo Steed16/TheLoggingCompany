427bb0cbdf05803cb853ec33a963d44381f66ab9
Cody Weiland <cweiland@cord.edu>
2018-12-17 10:36:24 -0600

    ds

--- TheLoggingCompanyProject/About_Cody.aspx.designer.cs
+++ TheLoggingCompanyProject/About_Cody.aspx.designer.cs
@@ -19,7 +19,7 @@ namespace TheLoggingCompanyProject {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::TheLoggingCompanyProject.architecture.Nav navHtml;
+        //protected global::TheLoggingCompanyProject.architecture.Nav navHtml;
         
         /// <summary>
         /// footerHtml control.
@@ -28,6 +28,6 @@ namespace TheLoggingCompanyProject {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::TheLoggingCompanyProject.architecture.Footer footerHtml;
+        //protected global::TheLoggingCompanyProject.architecture.Footer footerHtml;
     }
 }
